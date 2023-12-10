6cff39c62d69cef1da0a7eefd68774346c0a7703
sharlinrahman <sharlin.rahman91@gmail.com>
2023-12-10 17:42:10 +0100

    Function for calcultating and displaying results with color.

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ const quizContainer = document.getElementById('quizContainer');
 const questionsUl = document.getElementById('questionsUl');
 const resultContainer = document.getElementById('resultContainer');
 const resultBtn = document.getElementById('resultBtn');
+const resetBtn = document.getElementById('resetBtn');
 
 const startQuiz = () => {
     questions.forEach((question, index) => {
@@ -52,13 +53,58 @@ const startQuiz = () => {
         li.innerHTML = `
         <span>${index + 1}. ${question.question}</span>
         <label>
-        <input type="radio" name="${index}" value="true">True
+        <input type="radio" name='${index}' value='true'>True
         </label>
         <label>
-        <input type="radio" name="${index}" value="false">False
+        <input type="radio" name='${index}' value='false'>False
         </label>
         `;
        questionsUl.appendChild(li);
         });
     }
+    //startQuiz();
+
+const showFinalResult = () => {
+    const playerAnswers = Array.from(document.querySelectorAll('input[type="radio"]:checked'))
+                                .map(input => input.value === 'true');
+    
+    const correctAnswers = questions.filter((question, index) => question.answer === playerAnswers[index]);
+    const score = (correctAnswers.length / questions.length) * 100;
+    
+    resultDiv.innerHTML = `<p>Du fick ${correctAnswers.length} av ${questions.length} rätt!</p>`
+
+    const resultList = document.createElement('ul');
+    correctAnswers.forEach((question, index) => {
+    const li = document.createElement('li');
+    li.textContent = ` ${index + 1}. ${question.question}`;
+    li.classList.add(question.answer ? 'correct' : 'incorrect');
+     resultList.appendChild(li);
+});
+
+    resultDiv.appendChild(resultList);
+    
+    let msg = '';
+    if (score<50) {
+    msg = 'Underkänd!';
+        resultContainer.style.color = "red";
+    } else if (score>=50 && score<=75) {
+        msg = 'Bra!';
+          resultContainer.style.color = "orange";
+    } else {
+         msg = 'Riktigt bra!';
+            resultContainer.style.color = "green";
+    }
+        resultMsg.textContent = msg;
+    }; 
+    
+const resetQuiz = () => {
+    resultMsg.textContent = "";
+    resultDiv.innerHTML = "";
+    questionsUl.innerHTML = "";
     startQuiz();
+};
+//Event listener for submitBtn
+resultBtn.addEventListener('click', showFinalResult);
+resetBtn.addEventListener('click', resetQuiz);
+
+startQuiz();
